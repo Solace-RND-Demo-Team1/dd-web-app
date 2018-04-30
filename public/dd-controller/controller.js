62cbef407db0df734fc8cdbcd527fa740e141ab5
Andrew MacKenzie <amackenzie@kan.solacesystems.com>
2018-04-30 10:04:32 -0400

    replaced old gamefiles with new build and started on unity callbacks

--- public/dd-controller/controller.js
+++ public/dd-controller/controller.js
@@ -14,3 +14,23 @@ function moveCar(carNum, s, a, footbreak, bump) {
 
     gameInstance.SendMessage('Vehicle-' + carNum, 'MoveVehicle', dataStruct);
   }
+
+// Handle Events Reported by Unity Game
+function killPlayer(playerId) {
+  window.alert("Player : " + playerId + " killed !");
+}
+function gameOptionsConfigured(maxPlayers) {
+  window.alert("Game Options: Max Players = " + maxPlayers);
+}
+function enteredGameLobby() {
+  window.alert("Entering Game Lobby");
+}
+function gameStarted(numStartingPlayers) {
+  window.alert("Game Started with " + numStartingPlayers + " players!");
+}
+function playerSubstituted(playerId, colour) {
+  window.alert("Player " + playerId + " substitued / Player Colour: " + colour);
+}
+function playerKilled(playerId) {
+  window.alert("Player : " + playerId + " killed !");
+}
